c57f350d5bbd05606226e9b27d5d7d636c8257fa
BeastBoy0704 <beastboy85208@gmail.com>
2026-07-02 21:37:28 +0530

    Added Permanent Dark Mode

--- script.js
+++ script.js
@@ -219,8 +219,22 @@ window.addEventListener("appinstalled", () => {
 // Theme Toggle
 // ==========================
 
+// ==========================
+// Theme Toggle + Save Theme
+// ==========================
+
 const themeBtn = document.getElementById("themeBtn");
 
+// Load saved theme
+if(localStorage.getItem("theme") === "dark"){
+
+    document.body.classList.add("dark-mode");
+
+    themeBtn.textContent = "☀️";
+
+}
+
+// Toggle Theme
 themeBtn.addEventListener("click", () => {
 
     document.body.classList.toggle("dark-mode");
@@ -229,9 +243,13 @@ themeBtn.addEventListener("click", () => {
 
         themeBtn.textContent = "☀️";
 
+        localStorage.setItem("theme","dark");
+
     }else{
 
         themeBtn.textContent = "🌙";
+
+        localStorage.setItem("theme","light");
 
     }
 
